8a5a3f52ba692adce5f1e4c2ffdebdfdf8cdcfb6
roehr <romy.ehrhardt@gmail.com>
2018-11-01 00:06:39 +0100

    refactor
    refactor

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -25,9 +25,10 @@ ApplicationSolar::ApplicationSolar(std::string const& resource_path)
  ,planet_object{}
  ,m_view_transform{glm::translate(glm::fmat4{}, glm::fvec3{0.0f, 0.0f, 4.0f})}
  ,m_view_projection{utils::calculate_projection_matrix(initial_aspect_ratio)}
-{ initializeSceneGraph();
-  initializeGeometry();
-  initializeShaderPrograms();
+{ initializeGeometry();
+initializeShaderPrograms();
+initializeSceneGraph();
+  
 }
 
 ApplicationSolar::~ApplicationSolar() {
